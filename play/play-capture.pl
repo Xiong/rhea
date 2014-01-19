08e14174dd3c08c5012f3aaaf293e84db54ba396
Xiong Changnian <xiong@cpan.org>
2014-01-18 22:59:15 -0800

    New play script.
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

---
+++ play/play-capture.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+#       play-capture.pl
+#       = Copyright 2014 Xiong Changnian  <xiong@cpan.org>   =
+#       = Free Software = Artistic License 2.0 = NO WARRANTY =
+
+use 5.014002;   # 5.14.2    # 2012  # pop $arrayref, copy s///r
+use strict;
+use warnings;
+
+use lib qw| lib |;
+use Error::Base;
+use Perl6::Form;
+use Test::More;
+use Test::Trap;
+
+#~ use Devel::Comments '###';
+#~ use Devel::Comments '###', ({ -file => 'debug.log' });                   #~
+
+## use
+#============================================================================#
+say "$0 Running...";
+
+
+say "Done.";
+exit;
+#----------------------------------------------------------------------------#
+
+
+#============================================================================#
+__END__     
