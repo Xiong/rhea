fd1763d4ae3d530e52180975b645148b0e5fe7f8
Xiong Changnian <xiong@cpan.org>
2014-01-30 20:54:58 -0800

    New play script play-yaml.pl
    * To generate YAML for more play.
    
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

---
+++ play/play-yaml.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+#       play-yaml.pl
+#       = Copyright 2014 Xiong Changnian  <xiong@cpan.org>   =
+#       = Free Software = Artistic License 2.0 = NO WARRANTY =
+
+use 5.014002;   # 5.14.2    # 2012  # pop $arrayref, copy s///r
+use strict;
+use warnings;
+
+use lib qw| lib ../lib |;
+use App::Rhea;
+
+#~ use Error::Base;
+#~ use Perl6::Form;
+#~ use Test::More;
+#~ use Test::Trap;
+
+#~ use Devel::Comments '###';
+#~ use Devel::Comments '###', ({ -file => 'debug.log' });                   #~
+
+## use
+#============================================================================#
+say "$0 Running...";
+
+
+
+
+say "Done.";
+exit;
+#----------------------------------------------------------------------------#
+
+
+#============================================================================#
+__END__     
